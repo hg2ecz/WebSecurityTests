75362248aa6995da869dd5107dc28c5bfa648ca4
Zsolt Krüpl <hg2ecz@ham.hu>
2019-11-01 14:53:15 +0100

    xss check add

--- xss-check-autoparam.py
+++ xss-check-autoparam.py
@@ -5,19 +5,28 @@ import os
 import sys
 
 # git clone https://github.com/s0md3v/XSStrike
-# ./xss-check-autoparam.py http://szerver.lan/vuln-examples/xss.php
+# ./xss-check-autoparam.py "" http://szerver.lan/vuln-examples/xss.php
+# ./xss-check-autoparam.py "id;name;age" http://szerver.lan/vuln-examples/xss.php
 
-for url in sys.argv[1:]:
-    pars = []
-    plines = os.popen("python3 XSStrike/xsstrike.py -u '%s' --params"%(url), "r").readlines()
+init_pars = sys.argv[1].split(";")
+
+for url in sys.argv[2:]:
+    pars = init_pars
+    plines = os.popen("python3 XSStrike/xsstrike.py -u '%s' --params"%(url)).readlines()
     for p in plines:
         if re.search("Heuristics found a potentially valid parameter:", p):
             pars.append(p.split(':')[1].split(' ')[1][5:-5])
-    print ("Vizsgált GET paraméterek:", pars)
 
+    par_set = set()
     for p in pars:
+        if p == '': continue
+        par_set.add(p)
+
+    print ("Vizsgált GET paraméterek:", par_set)
+    for p in par_set:
         url_w_param = "%s?%s="%(url, p)
-        xss = os.popen("echo | python3 XSStrike/xsstrike.py -u '%s'"%(url_w_param), "r").read()
+        xss = os.popen("echo | python3 XSStrike/xsstrike.py -u '%s'"%(url_w_param)).read()
         #print (xss) # debug
-        if re.search("Reflections found:", xss) or re.search("Potentially vulnerable objects found", xss):
+        if re.search("Reflections found:", xss) or \
+           re.search("Potentially vulnerable objects found", xss):
             print(url_w_param, " is vulnerable with XSS")
